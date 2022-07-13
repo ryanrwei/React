f0e681ffb55a9442b9d318afa2fee7cf7ae1418d
Henry Johnson <jo74705@gmail.com>
2022-07-13 17:19:59 +0800

    Enhance code

--- src/App.js
+++ src/App.js
@@ -1,76 +1,79 @@
-import React, { useState } from 'react';
-// import {Component, useState} from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 import Header from './components/Header/Header'
 import List from './components/List/List'
-import Login from './components/Login/Login'
 
-const App =()=>{
+const App = () => {
+  let [listData, setListData] = useState({
+    listName: 'ryan',
+    listEmail: 'xxx@com',
+    listPassword: '123'
+  })
 
-    let [listData, setListData] = useState({
-        listName:'ryan',
-        listEmail:'xxx@com',
-        listPassword:'123',
+  let [isVisible, setvisibility] = useState(false)
+  let [isSuccess, setIsSuccess] = useState(false)
+
+  const updateListName = newData => {
+    setListData(preState => {
+      return {
+        ...preState,
+        listName: newData
+      }
     })
+  }
+  const updateListEmail = newData => {
+    setListData(preState => {
+      return {
+        ...preState,
+        listEmail: newData
+      }
+    })
+  }
+  const updateListPassword = newData => {
+    setListData(preState => {
+      return {
+        ...preState,
+        listPassword: newData
+      }
+    })
+  }
 
-    let [isVisible, setvisibility] = useState('hidden')
-
-
-	const updateListName = (newData)=>{
-        setListData((preState) => {
-            return {
-                ...preState,
-                listName:newData
-            }
-        })
-	}
-	const updateListEmail = (newData)=>{
-        setListData((preState) => {
-            return {
-                ...preState,
-                listEmail:newData
-            }
-        })	
-    }
-    const updateListPassword = (newData)=>{
-        setListData((preState) => {
-            return {
-                ...preState,
-                listPassword:newData
-            }
-        })	
-	}    
-
-    const changeVisibility = (newData) =>{
-        setvisibility({isVisible:newData})
-        console.log(isVisible)
-    }
-
-    return(
+  return (
     <div className='wholePage'>
-        <div className="app">
+      {isSuccess ? (
+        <div>Success</div>
+      ) : (
+        <div className='app'>
+          <div className='header'>
+            <Header />
+          </div>
+          {isVisible && (
+            <div className='passwordWrong'>Details do not match!</div>
+          )}
 
-            <div className="header">
-                <Header/>
-            </div>
+          <div className='list'>
+            <List
+              listData={listData}
+              updateListName={updateListName}
+              updateListEmail={updateListEmail}
+              updateListPassword={updateListPassword}
+            />
+          </div>
 
-            <div className='passwordWrong' style={{visibility:{isVisible}}}>Details do not match!</div>
-
-            <div className="list">
-                <List listData={listData} 
-                updateListName={updateListName}
-                updateListEmail={updateListEmail}
-                updateListPassword={updateListPassword}
-                />
-            </div>
-            
-            <div className="login">
-                <Login listData={listData} changeVisibility={changeVisibility}/>
-            </div>                               
-
+          <div>
+            <button
+              onClick={() => {
+                // setvisibility(true)
+                setIsSuccess(true)
+              }}
+            >
+              Login
+            </button>
+          </div>
         </div>
-
-    </div>)
+      )}
+    </div>
+  )
 }
 
-export default App;
+export default App
